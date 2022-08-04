6f8251be6abfa3cd139a19feaecf23e4893f1d8c
mamun5 <mamun@w3dev.email>
2022-08-04 21:41:18 +0600

    update

--- main.js
+++ main.js
@@ -19,37 +19,7 @@ const sphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50), new THREE.Mes
   map: new THREE.TextureLoader().load(pngEarth),
   color:0x0C2E4E
 }));
-// Create 60000 tiny dots and spiral them around the sphere.
-const DOT_COUNT = 60000;
 
-// A hexagon with a radius of 2 pixels looks like a circle
-const dotGeometry = new THREE.CircleGeometry(2, 5);
-
-// The XYZ coordinate of each dot
-const positions = [];
-
-// A random identifier for each dot
-const rndId = [];
-
-// The country border each dot falls within
-const countryIds = [];
-
-const vector = new THREE.Vector3();
-
-for (let i = DOT_COUNT; i >= 0; i--) {
-  const phi = Math.acos(-1 + (2 * i) / DOT_COUNT);
-  const theta = Math.sqrt(DOT_COUNT * Math.PI) * phi;
-
-  // Pass the angle between this dot an the Y-axis (phi)
-  // Pass this dot’s angle around the y axis (theta)
-  // Scale each position by 600 (the radius of the globe)
-  vector.setFromSphericalCoords(600, phi, theta);
-  dotGeometry.lookAt(vector);
-
-  // Move the dot to the newly calculated position
-  dotGeometry.translate(vector.x, vector.y, vector.z);
-
-}
 
 scene.add(sphere)
 camera.position.z = 11
